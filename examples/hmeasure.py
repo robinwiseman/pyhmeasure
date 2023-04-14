fe300f5c08ac17c8ba8bfb95ba8b4bea32b02fe2
Robin Wiseman <robin.wiseman@googlemail.com>
2023-04-14 12:48:40 +0100

    expose all H-Measure results

--- examples/hmeasure.py
+++ examples/hmeasure.py
@@ -5,5 +5,5 @@ c1scores = [0.3, 0.35, 0.36, 0.42, 0.5, 0.8, 0.82, 0.99]
 cd_alpha = 2.0
 cd_beta = 2.0
 hm = pyhmeasure.PyHmeasure(c0scores, c1scores, cd_alpha, cd_beta)
-h = hm.h_measure()
+print(f"H-Measure:{hm.h}")
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,18 @@
 use pyo3::prelude::*;
+use pyo3::types::PyList;
+
 
 use hmeasure::{CostRatioDensity, HMeasureResults, HMeasure};
 use hmeasure::{BetaParams, BinaryClassScores};
 
 #[pyclass]
 pub struct PyHmeasure {
-    pub scores: BinaryClassScores,
-    pub cost_density_alpha: f64,
-    pub cost_density_beta: f64
+    pub h: f64,
+    pub convex_hull: Vec<Vec<f64>>,
+    pub roc_curve: Vec<Vec<f64>>,
+    pub int_components: Vec<Vec<f64>>,
+    pub class0_score: Vec<f64>,
+    pub class1_score: Vec<f64>
 }
 
 #[pymethods]
@@ -15,25 +20,76 @@ impl PyHmeasure {
     #[new]
     pub fn new(c0scores: Vec<f64>, c1scores: Vec<f64>,
                cost_density_alpha: f64, cost_density_beta: f64) -> PyHmeasure {
-        let bcs = BinaryClassScores {
-            class0: c0scores,
-            class1: c1scores
-        };
-
-        PyHmeasure {
-            scores: bcs,
-            cost_density_alpha: cost_density_alpha,
-            cost_density_beta: cost_density_beta
-        }
-    }
-    pub fn h_measure(&mut self) -> f64 {
-        let beta_params = BetaParams { alpha: self.cost_density_alpha,
-                                       beta: self.cost_density_beta };
+        let mut bcs = BinaryClassScores { class0: c0scores,
+                                          class1: c1scores };
+        let beta_params = BetaParams { alpha: cost_density_alpha,
+                                       beta: cost_density_beta };
         let crd = CostRatioDensity::new(beta_params);
         // calculate the H-Measure given the cost ratio density and scores
         let mut hm = HMeasure::new(crd, None, None);
-        let hmr = hm.h_measure(&mut self.scores);
-        hmr.h
+        let hmr = hm.h_measure(&mut bcs);
+        PyHmeasure {
+                    h: hmr.h,
+                    convex_hull: hmr.convex_hull,
+                    roc_curve: hmr.roc_curve,
+                    int_components: hmr.int_components,
+                    class0_score: hmr.class0_score,
+                    class1_score: hmr.class1_score
+                }
+    }
+    #[getter]
+    fn h(&self) -> f64 {
+        self.h
+    }
+    #[getter]
+    fn class0_score(&self) -> PyResult<Py<PyList>> {
+        self.Vec_to_List("class0_score")
+    }
+    #[getter]
+    fn class1_score(&self) -> PyResult<Py<PyList>> {
+        self.Vec_to_List("class1_score")
+    }
+    #[getter]
+    fn convex_hull(&self) -> PyResult<Py<PyList>> {
+        self.VecVec_to_List("convex_hull")
+    }
+    #[getter]
+    fn roc_curve(&self) -> PyResult<Py<PyList>> {
+        self.VecVec_to_List("roc_curve")
+    }
+    fn Vec_to_List(&self, member: &str) -> PyResult<Py<PyList>> {
+        let missing= vec![0.0];
+        let vec = match member {
+            "class0_score" => &self.class0_score,
+            "class1_score" => &self.class1_score,
+            _ => &missing
+        };
+        Python::with_gil(|py| {
+            let list = PyList::empty(py);
+            for item in vec {
+                list.append(item);
+            }
+            Ok(list.into())
+        })
+    }
+    fn VecVec_to_List(&self, member: &str) -> PyResult<Py<PyList>> {
+        let missing= vec![vec![0.0]];
+        let vec = match member {
+            "convex_hull" => &self.convex_hull,
+            "roc_curve" => &self.roc_curve,
+            _ => &missing
+        };
+        Python::with_gil(|py| {
+            let list = PyList::empty(py);
+            for inner_vec in vec {
+                let innerlist = PyList::empty(py);
+                for item in inner_vec {
+                    innerlist.append(item);
+                }
+                list.append(innerlist);
+            }
+            Ok(list.into())
+        })
     }
 }
 
